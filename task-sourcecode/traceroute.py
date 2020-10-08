53cb78b0a7d157b7cc93f185ef5599279fd28d08
saurabh-172 <saurabh300.gole@gmail.com>
2020-10-08 05:04:59 +0000

    correcting source code

--- task-sourcecode/traceroute.py
+++ task-sourcecode/traceroute.py
@@ -1,32 +1,35 @@
 import socket
 import sys
 
+#setting Time-To-Live for sending packets
 ttl = 1
-
+#getting the IP address of the host to which message is being sent
 host_IP = socket.gethostbyname(sys.argv[1])
 
-def send():
+def send(ttl):
+    #creating UDP scoket
     sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    #setting ttl for the message to be sent
     sock.setsockopt(socket.IPPROTO_IP,socket.IP_TTL,ttl)
+    #sending the message
     sock.sendto(b"",(host_IP,5000))
 
 def receive():
     ttl=1
-    # Raw packet sending icmp echo request
+    # creating raw socket for receiving icmp messages
     sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
     print("listening...")
-    while(sock.setsockopt(socket.SOL_IP,socket.IP_HDRINCL,ttl)==0):
-        print(ttl)
+    #listening to incoming icmp messages
+    while(1):
+        #receiving the data and addresss of the router sending it
         data,addr = sock.recvfrom(1024)
         print(f"{addr[0]}")
+        #if received ip address equal to the final destination host IP then stop listening
+        if addr[0]==host_IP:
+            break
+        #if not then increament ttl and send
         ttl+=1
-    print(ttl)
-'''
-    while True:
-        data,addr = sock.recvfrom(1024)
-        print(f"reply received from {addr}")
-'''
-send()
+        send(ttl)
+send(ttl)
 receive()
 
-'''setsockopt(): is used to control socket behaviour like allocate buffer size, control timeout etc'''
