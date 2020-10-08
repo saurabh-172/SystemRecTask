5ed94264ec5668df50b06a9e8323fd1fd8bb768f
saurabh-172 <saurabh300.gole@gmail.com>
2020-10-08 06:01:54 +0530

    correction in receiving icmp packets

--- task-sourcecode/traceroute.py
+++ task-sourcecode/traceroute.py
@@ -6,20 +6,26 @@ ttl = 1
 host_IP = socket.gethostbyname(sys.argv[1])
 
 def send():
-    sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
-    sock.setsockopt(socket.IPPROTO_ICMP,socket.IP_HDRINCL,ttl)
-    sock.sendto(b"",(host_IP,80))
+    sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    sock.setsockopt(socket.IPPROTO_IP,socket.IP_TTL,ttl)
+    sock.sendto(b"",(host_IP,5000))
 
 def receive():
+    ttl=1
     # Raw packet sending icmp echo request
     sock = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket.IPPROTO_ICMP)
-    sock.setsockopt(socket.IPPROTO_ICMP,socket.IP_HDRINCL,ttl)
-    sock.bind(("127.0.0.1",80))
-
+    print("listening...")
+    while(sock.setsockopt(socket.SOL_IP,socket.IP_HDRINCL,ttl)==0):
+        print(ttl)
+        data,addr = sock.recvfrom(1024)
+        print(f"{addr[0]}")
+        ttl+=1
+    print(ttl)
+'''
     while True:
         data,addr = sock.recvfrom(1024)
         print(f"reply received from {addr}")
-
+'''
 send()
 receive()
 
